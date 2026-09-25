dd24c6e3a9e7febbb4dd1b06363da553d6fb3eb9
sumitkshaw <shawsumit6286@gmail.com>
2026-09-25 21:55:00 +0530

    feat: add platform admin layout shell and expandable sidebar components

--- src/app/platform-admin/components/PlatformAdminShell.tsx
+++ src/app/platform-admin/components/PlatformAdminShell.tsx
@@ -41,7 +41,7 @@ export function PlatformAdminShell({
 
           <Link href="/platform-admin" className="flex items-center">
             <img 
-              src="/z i g z a (7).png" 
+              src="/z i g z a (8).png" 
               alt="Zigza" 
               className="h-8 sm:h-9 w-auto object-contain"
             />
